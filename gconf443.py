df651590f24b803de7300906897dc6414a5d6d7d
wenyamu <568640717@qq.com>
2023-08-22 22:22:27 +0800

    ssl bug

--- gconf443.py
+++ gconf443.py
@@ -41,7 +41,7 @@ raw_env=["ljs1=111","ljs2=222"]
 
 #gunicorn 配置 ssl
 keyfile = '/ssl/privkey.pem'
-certfile = '/ssl/689890/fullchain.pem'
+certfile = '/ssl/fullchain.pem'
 
 loglevel = 'info' #日志级别，这个日志级别指的是错误日志的级别，而访问日志的级别无法设置
 access_log_format = '%(t)s %(p)s %(h)s "%(r)s" %(s)s %(L)s %(b)s %(f)s" "%(a)s"'#设置gunicorn访问日志格式，错误日志无法设置
